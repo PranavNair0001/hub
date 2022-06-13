687c3e744d4240dba5d3f9cc03d6b85aec857367
PranavNair0001 <pranavnair@google.com>
2022-06-13 05:32:54 +0000

    Fix artifact merge

--- .github/scripts/merge.sh
+++ .github/scripts/merge.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -a toFetch;
+declare -a missing;
 
 for artifact in packages/* ; do
     for artifactVersion in ${artifact}/* ; do
@@ -25,20 +26,37 @@ for artifact in packages/* ; do
 done
 
 for file in ${toFetch[@]} ; do
-    filePath="artifacts/`echo $file | cut -d "/" -f 4`";
-    echo "${filePath} :";
-    cat ${filePath} | less
-    cd ${filePath}
-    ls
-    cd ../../
-    if [ -f "${filePath}" ]; then
-        mv ${filePath} $file
+    fileName=`echo $file | cut -d "/" -f 4`;
+    if [ -f "artifacts/${fileName}/${fileName}" ]; then
+        mv artifacts/${fileName}/${fileName} $file
     else
         echo "$file : not retrieved"
     fi
 done
 
+for artifact in packages/* ; do
+    for artifactVersion in ${artifact}/* ; do
+        if [ ! -f "${artifactVersion}/spec.json" ]; then
+            echo "${artifactVersion}/spec.json does not exist";
+            exit 0;
+        else
+            jarFiles=($(jq -r '.actions[] | select(.type == "one_step_deploy_plugin").arguments[] | select(.name == "jar").value' ${artifactVersion}/spec.json));
+            configFiles=($(jq -r '.actions[] | select(.type == "one_step_deploy_plugin").arguments[] | select(.name == "config").value' ${artifactVersion}/spec.json));
+            for jarFile in $jarFiles ; do
+                if [ ! -f "${artifactVersion}/${jarFile}" ]; then
+                    missing[${#toFetch[@]}]=${artifactVersion}/${jarFile}
+                fi
+            done
+            for configFile in $configFiles ; do
+                if [ ! -f "${artifactVersion}/${configFile}" ]; then
+                    missing[${#toFetch[@]}]=${artifactVersion}/${configFile}
+                fi
+            done
+        fi
+    done
+done
+
 echo "Missing files after retrieval: "
-for file in ${toFetch[@]} ; do
+for file in ${missing[@]} ; do
     echo $file;
 done
