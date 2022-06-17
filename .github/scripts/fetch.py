cd1c644725a3398314483bf78e02d11b7d47399b
PranavNair0001 <pranavnair@google.com>
2022-06-17 13:03:34 +0000

    Update fetch.py

--- .github/scripts/fetch.py
+++ .github/scripts/fetch.py
@@ -12,7 +12,14 @@ for file in toFetch:
 for id in ids:
   print(id)
 
+jsonStr = ''
 for i in range(len(toFetch)):
   filename = utilities.run_shell_command('basename -- ' + toFetch[i])
   extension = utilities.run_shell_command('filename=' + filename + ' && ${filename##*.}')
+  jsonStr += '{"path":"' + toFetch[i] + '","target_path":"artifact/' + toFetch[i].split('/')[3] + '","artifact":"' + toFetch[i].split('/')[3] + '","repo":{"id":"' + ids[i] + '","file_type":"' + extension + '"}},'
 
+output = '[' + jsonStr[:-1] + ']'
+print('Output of fetch.py: ')
+print(output)
+
+utilities.run_shell_command('output=' + output + ' && export output && echo "output=${output}" >> $GITHUB_ENV')
--- .github/scripts/utilities.py
+++ .github/scripts/utilities.py
@@ -4,56 +4,56 @@ import json
 import yaml
 
 def run_shell_command(cmd):
-    process = subprocess.run(cmd, stderr=subprocess.PIPE, shell=True)
-    if(process.returncode != 0):
-        print('Process completed with error: ', process.stderr)
-    assert process.returncode == 0
+  process = subprocess.run(cmd, stderr=subprocess.PIPE, shell=True)
+  if(process.returncode != 0):
+    print('Process completed with error: ', process.stderr)
+  assert process.returncode == 0
 
 def get_missing_files():
-    files = []
-    ids = []
-    packagesDir = 'packages/'
-    for artifact in os.listdir(packagesDir):
-        artifactDir = os.path.join(packagesDir, artifact)
-        if(os.path.isdir(artifactDir)):
-            for version in os.listdir(artifactDir):
-                artifactVersionDir = os.path.join(artifactDir, version)
-                if(os.path.isdir(artifactVersionDir)):
-                    if(os.path.isfile(os.path.join(artifactVersionDir, 'spec.json'))):
-                        specFile = open(os.path.join(artifactVersionDir, 'spec.json'))
-                        specData = json.load(specFile)
-                        jarFiles = []
-                        configFiles = []
-                        for object in specFile['actions']:
-                            if(object['type'] == 'one_step_deploy_plugin'):
-                                for property in object['arguments']:
-                                    if(property['name'] == 'jar'):
-                                        jarFiles.append(property['value'])
-                                    if(property['name'] == 'config'):
-                                        configFiles.append(property['value'])
-                        if((len(jarFiles) != 0) or (len(configFiles != 0))):
-                            if(os.path.isfile(artifactDir, 'build.yaml')):
-                                buildFile = open(os.path.join(artifactDir, 'build.yaml'))
-                                buildData = yaml.load(buildFile, Loader=yaml.FullLoader)
-                                groupId = buildData['maven-central']['groupId']
-                                artifactId = buildData['maven-central']['artifactId']
-                                for jarFile in jarFiles:
-                                    if(not os.path.isfile(os.path.join(artifactVersionDir, jarFile))):
-                                        files.append(os.path.join(artifactVersionDir, jarFile))
-                                        ids.append('%s:%s:%s' %(groupId, artifactId, version))
-                                for configFile in configFiles:
-                                    if(not os.path.isfile(os.path.join(artifactVersionDir, configFile))):
-                                        files.append(os.path.join(artifactVersionDir, configFile))
-                                        ids.append('%s:%s:%s' %(groupId, artifactId, version))
-                            else:
-                                print('Error: build.yaml file does not exist for ' + artifactDir)
-                    else:
-                        print('Error: spec.json does not exist for ' + artifactVersionDir)
-                        exit(1)
-    return files, ids
+  files = []
+  ids = []
+  packagesDir = 'packages/'
+  for artifact in os.listdir(packagesDir):
+    artifactDir = os.path.join(packagesDir, artifact)
+    if(os.path.isdir(artifactDir)):
+      for version in os.listdir(artifactDir):
+        artifactVersionDir = os.path.join(artifactDir, version)
+        if(os.path.isdir(artifactVersionDir)):
+          if(os.path.isfile(os.path.join(artifactVersionDir, 'spec.json'))):
+            specFile = open(os.path.join(artifactVersionDir, 'spec.json'))
+            specData = json.load(specFile)
+            jarFiles = []
+            configFiles = []
+            for object in specFile['actions']:
+              if(object['type'] == 'one_step_deploy_plugin'):
+                for property in object['arguments']:
+                  if(property['name'] == 'jar'):
+                    jarFiles.append(property['value'])
+                  if(property['name'] == 'config'):
+                    configFiles.append(property['value'])
+            if((len(jarFiles) != 0) or (len(configFiles != 0))):
+              if(os.path.isfile(artifactDir, 'build.yaml')):
+                buildFile = open(os.path.join(artifactDir, 'build.yaml'))
+                buildData = yaml.load(buildFile, Loader=yaml.FullLoader)
+                groupId = buildData['maven-central']['groupId']
+                artifactId = buildData['maven-central']['artifactId']
+                for jarFile in jarFiles:
+                  if(not os.path.isfile(os.path.join(artifactVersionDir, jarFile))):
+                    files.append(os.path.join(artifactVersionDir, jarFile))
+                    ids.append('%s:%s:%s' %(groupId, artifactId, version))
+                for configFile in configFiles:
+                  if(not os.path.isfile(os.path.join(artifactVersionDir, configFile))):
+                    files.append(os.path.join(artifactVersionDir, configFile))
+                    ids.append('%s:%s:%s' %(groupId, artifactId, version))
+              else:
+                print('Error: build.yaml file does not exist for ' + artifactDir)
+          else:
+            print('Error: spec.json does not exist for ' + artifactVersionDir)
+            exit(1)
+  return files, ids
 
 def gcs_sync_dir(source, destination, ignore=False):
-    if ignore:
-        run_shell_command('gsutil -m rsync -i -r %s %s' %(source, destination))
-    else:
-        run_shell_command('gsutil -m rsync -r %s %s' %(source, destination))
+  if ignore:
+    run_shell_command('gsutil -m rsync -i -r %s %s' %(source, destination))
+  else:
+    run_shell_command('gsutil -m rsync -r %s %s' %(source, destination))
