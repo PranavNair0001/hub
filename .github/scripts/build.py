127a3c0ff96035411840ba5d42d1c27b57125f0f
PranavNair0001 <pranavnair@google.com>
2022-06-17 05:36:25 +0000

    Create build.py

---
+++ .github/scripts/build.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+def run_shell_command(cmd):
+    process = subprocess.run(cmd.split(' '), stderr=subprocess.PIPE)
+    if(process.returncode != 0):
+        print('Process completed with error: ', process.stderr)
+    assert process.returncode == 0
+
+print('Starting Setup and Build of Packager, packages.json  ....')
+print('Checking environment ....\n')
+
+print('Java version: ')
+run_shell_command('java -version')
+
+print('Java compile version: ')
+run_shell_command('javac -version')
+
+print('Maven version: ')
+run_shell_command('mvn -version')
+
+print('Building packager ....\n')
+os.chdir('./packager/')
+run_shell_command('mvn clean package')
+os.chdir('../')
+
+print('Building packages.json ....\n')
+run_shell_command('java -cp "packager/target/lib/*:packager/target/*" io.cdap.hub.Tool build')
+
+print('ls:\n', os.listdir())
