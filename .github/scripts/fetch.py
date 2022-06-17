0c89fff5e9c5f2a75f01f4bbe86f761394b15f71
PranavNair0001 <pranavnair@google.com>
2022-06-17 08:08:58 +0000

    Create fetch.py

--- .github/scripts/fetch.py
+++ .github/scripts/fetch.py
@@ -3,6 +3,16 @@ import utilities
 
 utilities.gcs_sync_dir('gs://project-step-pranav-hub-cdap-io/packages/', './packages/')
 
-toFetch = []
-ids = []
+toFetch, ids = utilities.get_missing_files()
+
+print('Missing files before retrieval are: ')
+for file in toFetch:
+  print(file)
+
+for id in ids:
+  print(id)
+
+for i in range(len(toFetch)):
+  filename = utilities.run_shell_command('basename -- ' + toFetch[i])
+  extension = utilities.run_shell_command('filename=' + filename + ' && ${filename##*.}')
 
