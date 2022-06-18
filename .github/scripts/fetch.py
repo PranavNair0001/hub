53eb349744d06b96900f830d834f544c90fee867
PranavNair0001 <pranavnair@google.com>
2022-06-18 06:11:43 +0000

    Add \ for json

--- .github/scripts/fetch.py
+++ .github/scripts/fetch.py
@@ -15,7 +15,7 @@ for id in ids:
 jsonStr = ''
 for i in range(len(toFetch)):
   extension = toFetch[i].split('.')[-1]
-  jsonStr += '{"path":"' + toFetch[i] + '","target_path":"artifact/' + toFetch[i].split('/')[3] + '","artifact":"' + toFetch[i].split('/')[3] + '","repo":{"id":"' + ids[i] + '","file_type":"' + extension + '"}},'
+  jsonStr += '{\"path\":\"' + toFetch[i] + '\",\"target_path\":\"artifact/' + toFetch[i].split('/')[3] + '\",\"artifact\":\"' + toFetch[i].split('/')[3] + '\",\"repo\":{\"id\":\"' + ids[i] + '\",\"file_type\":\"' + extension + '\"}},'
 
 output = '[' + jsonStr[:-1] + ']'
 print('Output of fetch.py: ')
