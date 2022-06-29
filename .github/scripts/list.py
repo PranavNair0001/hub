71e4123b0704cbb9428b14e4d83731dd2ff9366f
grace-matson <gracematson@google.com>
2022-06-29 04:29:40 +0000

    Document list.py, Logging list.py

--- .github/scripts/list.py
+++ .github/scripts/list.py
@@ -1,25 +1,39 @@
 import os
 import utilities
+import logging
+import json
 
+logging.getLogger().setLevel(logging.INFO)
 utilities.gcs_sync_dir('gs://project-step-pranav-hub-cdap-io/packages/', './packages/', ignore=True)
 
 toFetch, ids = utilities.get_missing_files()
 
-print('Missing files before retrieval are: ')
+logging.info('Missing files before retrieval are: ')
 for file in toFetch:
-  print(file)
+  logging.info(file)
 
 for id in ids:
-  print(id)
+  logging.info(id)
 
 jsonStr = ''
 for i in range(len(toFetch)):
-  extension = toFetch[i].split('.')[-1]
-  jsonStr += '{\"path\":\"' + toFetch[i] + '\",\"target_path\":\"artifact/' + toFetch[i].split('/')[3] + '\",\"artifact\":\"' + toFetch[i].split('/')[3] + '\",\"repo\":{\"id\":\"' + ids[i] + '\",\"file_type\":\"' + extension + '\"}},'
+  #example obj : packages/hydrator-plugin-gcp-plugins/0.17.4/google-cloud-0.17.4.jar
+  obj = toFetch[i]
+  extension = obj.split('.')[-1]
+  file_name = obj.split('/')[3]
+  jsonStr += '{' + \
+             '"path":\"' + obj + \
+             '\","target_path":\"artifact/' + file_name + \
+             '\","artifact":\"' + file_name + \
+             '\","repo": {"id":\"' + ids[i] + \
+             '\","file_type":\"' + extension + \
+             '\"}},'
 
-output = '[' + jsonStr[:-1] + ']'
+output = '{"output":[' + jsonStr[:-1] + ']}'
 print('Output of list.py: ')
-print(output)
+json_object = json.loads(output)
+json_formatted_str = json.dumps(json_object, indent=2)
+print(json_formatted_str)
 
 env_file = os.getenv('GITHUB_ENV')
 
