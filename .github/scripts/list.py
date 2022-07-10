cba9ca7995c58169e155e0a7bf112d4119aee09a
PranavNair0001 <pranavnair@google.com>
2022-07-10 07:18:20 +0000

    Modify documentation, fix syntax

--- .github/scripts/list.py
+++ .github/scripts/list.py
@@ -1,7 +1,7 @@
 import json
 import os
-import utilities
 import logging
+import utilities
 
 logging.getLogger().setLevel(logging.INFO)    # Enable logging in GitHub Workflow
 
@@ -17,27 +17,14 @@ for id in ids:
 
 jsonStr = ''
 for i in range(len(toFetch)):
-  #example obj : packages/hydrator-plugin-gcp-plugins/0.17.4/google-cloud-0.17.4.jar
-  obj = toFetch[i]
-  extension = obj.split('.')[-1]
-  file_name = obj.split('/')[3]
-  jsonStr += '{' + \
-             '"path":\"' + obj + \
-             '\","target_path":\"artifact/' + file_name + \
-             '\","artifact":\"' + file_name + \
-             '\","repo": {"id":\"' + ids[i] + \
-             '\","file_type":\"' + extension + \
-             '\"}},'
+  extension = toFetch[i].split('.')[-1]
+  jsonStr += '{\"path\":\"' + toFetch[i] + '\",\"target_path\":\"artifact/' + toFetch[i].split('/')[3] + '\",\"artifact\":\"' + toFetch[i].split('/')[3] + '\",\"artifactDir\":\"' + toFetch[i].rsplit('/', 1)[0] + '\",\"repo\":{\"id\":\"' + ids[i] + '\",\"file_type\":\"' + extension + '\"}},'
 
-output = '{"output":[' + jsonStr[:-1] + ']}'
+output = '[' + jsonStr[:-1] + ']'
 logging.info('Output of list.py: ')
 logging.info(json.dumps(json.loads(output), indent=2))    # Pretty print JSON output
-output = '[' + jsonStr[:-1] + ']'
 
 # Set output as environment variable of GitHub workflow runner
 env_file = os.getenv('GITHUB_ENV')
-
 with open(env_file, "a") as myfile:
   myfile.write("output=" + str(output))
-
-
