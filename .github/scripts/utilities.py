134a202406b55d3d1fee4b111692dfd43d8f7c76
PranavNair0001 <pranavnair@google.com>
2022-07-13 14:48:38 +0000

    Add output

--- .github/scripts/utilities.py
+++ .github/scripts/utilities.py
@@ -18,7 +18,7 @@ class LazyDecoder(json.JSONDecoder):
     return super().decode(s, **kwargs)
 
 def run_shell_command(cmd):
-  process = subprocess.run(cmd, stderr=subprocess.PIPE, shell=True, stdout=subprocess.STDOUT)
+  process = subprocess.run(cmd, stderr=subprocess.PIPE, shell=True, stdout=subprocess.PIPE)
   if(process.returncode != 0):
     print('Process completed with error: ', process.stderr)
   assert process.returncode == 0
