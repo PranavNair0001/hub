bfa3cd641ccaac846288b08a11a06c7ef3d1aa2f
PranavNair0001 <pranavnair@google.com>
2022-07-19 15:27:29 +0000

    Update merge.py

--- .github/scripts/merge.py
+++ .github/scripts/merge.py
@@ -1,3 +1,17 @@
+# Copyright © 2022 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
 import os
 import shutil
 import logging
@@ -7,15 +21,19 @@ logging.getLogger().setLevel(logging.DEBUG)    # Enable logging in GitHub Workfl
 
 toFetch, ids = utilities.get_missing_files()
 
+# Every missing file downloaded in ./artifact directory is placed in appropriate location
 for file in toFetch:
   fileName = file.split('/')[3]
   logging.info('Merging missing file: ' + fileName)
   if(os.path.isfile(os.path.join('artifacts', fileName, fileName))):
     shutil.move(os.path.join('artifacts', fileName, fileName), file)
   else:
-    logging.info('WARNING: ' + file + ' : not retrieved')
+    logging.warning(file + ' : not retrieved')
 
 shutil.move(os.path.join('artifacts', 'packages.json', 'packages.json'), 'packages.json')
+
+if(os.path.isdir('artifacts')):
+  shutil.rmtree('artifacts')
 
 toFetch, ids = utilities.get_missing_files()
 
@@ -24,8 +42,6 @@ for file in toFetch:
   logging.info(file)
 
 if(len(toFetch) != 0):
-  logging.info('Above file(s) yet to be fetched')
-  exit(1)
+  logging.error('Above file(s) yet to be fetched')
 
-if(os.path.isdir('artifacts')):
-  shutil.rmtree('artifacts')
+assert len(toFetch) == 0
