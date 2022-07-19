8f880629234e28b1202cde69912185e71ce20735
PranavNair0001 <pranavnair@google.com>
2022-07-19 10:01:08 +0000

    Add -c in rsync

--- .github/scripts/sync.py
+++ .github/scripts/sync.py
@@ -1,5 +1,5 @@
 import utilities
-utilities.run_shell_command('gsutil -m rsync -d -i -r packages/ gs://project-step-pranav-hub-cdap-io/packages/')
+utilities.run_shell_command('gsutil -m rsync -d -c -r packages/ gs://project-step-pranav-hub-cdap-io/packages/')
 utilities.run_shell_command('gsutil cp categories.json gs://project-step-pranav-hub-cdap-io/categories.json')
 utilities.run_shell_command('gsutil cp packages.json gs://project-step-pranav-hub-cdap-io/packages.json')
-utilities.run_shell_command('gsutil -m rsync -d -i -r gs://project-step-pranav-hub-cdap-io/ gs://project-step-pranav-hub-cdap-io-master/')
+utilities.run_shell_command('gsutil -m rsync -d -c -r gs://project-step-pranav-hub-cdap-io/ gs://project-step-pranav-hub-cdap-io-master/')
