ffdad8aedbb84173bc1d41368bb139c18514fe5a
PranavNair0001 <pranavnair@google.com>
2022-07-19 15:30:57 +0000

    Update utilities.py

--- .github/scripts/utilities.py
+++ .github/scripts/utilities.py
@@ -1,3 +1,17 @@
+# Copyright © 2022 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
 import os
 import subprocess
 import shlex
@@ -8,23 +22,15 @@ import logging
 
 logging.getLogger().setLevel(logging.DEBUG)    # Enable logging in GitHub Workflow and enable printing of info level logs
 
-class LazyDecoder(json.JSONDecoder):
+class LazyDecoder(json.JSONDecoder):    # Brute force RE based methods to decode JSON into appropriate format
   def decode(self, s, **kwargs):
     regex_replacements = [
-        (re.compile(r'([^\\])\\([^\\])'), r'\1\\\\\2'),
-        (re.compile(r',(\s*])'), r'\1'),
+          (re.compile(r'([^\\])\\([^\\])'), r'\1\\\\\2'),
+          (re.compile(r',(\s*])'), r'\1'),
     ]
     for regex, replacement in regex_replacements:
       s = regex.sub(replacement, s)
     return super().decode(s, **kwargs)
-
-def run(command):
-  process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-  while True:
-    line = process.stdout.readline().rstrip()
-    if not line:
-      break
-    yield line.decode('utf-8')
 
 def run_shell_command(command):    # Utility function to run shell commands with appropriate I/O streams
   process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
@@ -41,7 +47,7 @@ def get_missing_files():    # Utility function to get list of missing artifact f
   files = []
   ids = []
   packagesDir = 'packages/'
-  ignorableActionTypes = ['create_driver_artifact']
+  ignorableActionTypes = ['create_driver_artifact']    # Actions in spec.json for which jar/config files is only logged as warning not error. Eg: hydrator-pipeline-mysql-to-bigquery/1.1.1/mysql-connector-java.json
 
   for artifact in os.listdir(packagesDir):
     artifactDir = os.path.join(packagesDir, artifact)
@@ -59,70 +65,41 @@ def get_missing_files():    # Utility function to get list of missing artifact f
             configFiles = []
 
             for object in specData['actions']:
-                for property in object['arguments']:
-                  if(property['name'] == 'jar'):
-                    if(object['type'] in ignorableActionTypes):
-                      if(not os.path.isfile(os.path.join(artifactVersionDir, property['value']))):
-                        logging.info('WARNING: ' + os.path.join(artifactVersionDir, property['value']) + ' not found')
-                    else:
-                      jarFiles.append(property['value'])
-                  if(property['name'] == 'config'):
-                    if(object['type'] in ignorableActionTypes):
-                      if(not os.path.isfile(os.path.join(artifactVersionDir, property['value']))):
-                        logging.info('WARNING: ' + os.path.join(artifactVersionDir, property['value']) + ' not found')
-                    else:
-                      configFiles.append(property['value'])
+              for property in object['arguments']:
+                if(property['name'] == 'jar'):
+                  if(object['type'] in ignorableActionTypes):
+                    if(not os.path.isfile(os.path.join(artifactVersionDir, property['value']))):
+                      logging.warning(os.path.join(artifactVersionDir, property['value']) + ' not found')
+                  else:
+                    jarFiles.append(property['value'])
+                if(property['name'] == 'config'):
+                  if(object['type'] in ignorableActionTypes):
+                    if(not os.path.isfile(os.path.join(artifactVersionDir, property['value']))):
+                      logging.warning(os.path.join(artifactVersionDir, property['value']) + ' not found')
+                  else:
+                    configFiles.append(property['value'])
 
             logging.info('Required files: ')
             logging.info(jarFiles)
             logging.info(configFiles)
 
-            for jarFile in jarFiles:
-              if(not os.path.isfile(os.path.join(artifactVersionDir, jarFile))):
-                if(os.path.isfile(os.path.join(artifactDir, 'build.yaml'))):
-                  buildFile = open(os.path.join(artifactDir, 'build.yaml'))
-                  buildData = yaml.load(buildFile, Loader=yaml.FullLoader)
-                  groupId = buildData['maven-central']['groupId']
-                  artifactId = buildData['maven-central']['artifactId']
-                  files.append(os.path.join(artifactVersionDir, jarFile))
-                  ids.append('%s:%s:%s' %(groupId, artifactId, version))
-                  logging.info('Missing file: ' + jarFile)
-                else:
-                  logging.info('WARNING: build.yaml file does not exist for ' + artifactDir)
-                  files.append(os.path.join(artifactVersionDir, jarFile))
-                  ids.append('::%s' %(version))
-
-            for configFile in configFiles:
-              if(not os.path.isfile(os.path.join(artifactVersionDir, configFile))):
-                if(os.path.isfile(os.path.join(artifactDir, 'build.yaml'))):
-                  buildFile = open(os.path.join(artifactDir, 'build.yaml'))
-                  buildData = yaml.load(buildFile, Loader=yaml.FullLoader)
-                  groupId = buildData['maven-central']['groupId']
-                  artifactId = buildData['maven-central']['artifactId']
-                  files.append(os.path.join(artifactVersionDir, configFile))
-                  ids.append('%s:%s:%s' %(groupId, artifactId, version))
-                  logging.info('Missing file: ' + configFile)
-                else:
-                  logging.info('WARNING: build.yaml file does not exist for ' + artifactDir)
-                  files.append(os.path.join(artifactVersionDir, configFile))
-                  ids.append('::%s' %(version))
-
+            for fileList in [jarFiles, configFiles]:
+              for file in fileList:
+                if(not os.path.isfile(os.path.join(artifactVersionDir, file))):
+                  if(os.path.isfile(os.path.join(artifactDir, 'build.yaml'))):
+                    buildFile = open(os.path.join(artifactDir, 'build.yaml'))
+                    buildData = yaml.load(buildFile, Loader=yaml.FullLoader)
+                    groupId = buildData['maven-central']['groupId']
+                    artifactId = buildData['maven-central']['artifactId']
+                    files.append(os.path.join(artifactVersionDir, file))
+                    ids.append('%s:%s:%s' %(groupId, artifactId, version))
+                    logging.info('Missing file: ' + file)
+                  else:
+                    logging.warning('build.yaml file does not exist for ' + artifactDir)
+                    files.append(os.path.join(artifactVersionDir, file))
+                    ids.append('::%s' %(version))
           else:
-            logging.info('ERROR: spec.json does not exist for ' + artifactVersionDir)
+            logging.error('spec.json does not exist for ' + artifactVersionDir)
             exit(1)
 
   return files, ids
-
-def gcs_sync_dir(source, destination, ignore=False):
-  logging.info('Syncing directory using gsutil. Source: ' + source + ' Destination: ' + destination)
-  if ignore:
-    run_shell_command('gsutil -m rsync -i -r %s %s' %(source, destination))
-  else:
-    run_shell_command('gsutil -m rsync -r %s %s' %(source, destination))
-
-def gcs_copy(source, destination, overwrite=False):
-  logging.info('Copying file using gsutil. Source: ' + source + ' Destination: ' + destination)
-  if overwrite:
-    run_shell_command('gsutil cp %s %s' %(source, destination))
-  else:
-    run_shell_command('gsutil cp -n %s %s' %(source, destination))
